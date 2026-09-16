aafd775619807c4c79a7483873dac75bedc2e04c
Anthony Lima <anthonny.and2.0@gmail.com>
2026-09-16 20:20:10 -0300

    cria formulário de cadastro de cursos

---
+++ frontend/src/app/beginning-form/beginning-form.component.spec.ts
@@ -0,0 +1,23 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { BeginningFormComponent } from './beginning-form.component';
+
+// describe('BeginningFormComponent', () => {
+//   let component: BeginningFormComponent;
+//   let fixture: ComponentFixture<BeginningFormComponent>;
+
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [BeginningFormComponent]
+//     })
+//     .compileComponents();
+
+//     fixture = TestBed.createComponent(BeginningFormComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
